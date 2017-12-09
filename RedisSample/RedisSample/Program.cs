ae6468ed4367bec38f72fe356e86cd5efeee7761
Zhao Guangtao <zhaoguangtao@outlook.com>
2017-12-09 15:42:55 +0800

    Initial commit.

---
+++ RedisSample/RedisSample/Program.cs
@@ -0,0 +1,15 @@
+﻿using ServiceStack.Redis;
+using System;
+
+namespace RedisSample
+{
+    class Program
+    {
+        static RedisClient redisClient = new RedisClient("127.0.0.1", 6379);
+        static void Main(string[] args)
+        {
+            Console.WriteLine(redisClient.Get<string>("city"));
+            Console.ReadKey();
+        }
+    }
+}
